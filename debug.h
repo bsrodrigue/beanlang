d45d78169a3dd9dc171ae56ed596d5ce6c73dea1
Rachid <rbadini@n7studio.com>
2023-01-30 07:48:21 +0000

    first commit

---
+++ debug.h
@@ -0,0 +1,10 @@
+#ifndef clox_debug_h
+#define clox_debug_h
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk *chunk, const char *name);
+int disassembleInstruction(Chunk *chunk, int offset);
+static int constantInstruction(const char *name, Chunk *chunk, int offset);
+
+#endif
