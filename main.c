b701987cea64026079b604b924d7361bd6281e72
Rachid <rbadini@n7studio.com>
2023-02-01 12:40:26 +0000

    feature: vm and additional opcode

--- main.c
+++ main.c
@@ -1,19 +1,26 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char *argv[])
 {
-    Chunk chunk;
+    initVM();
 
+    Chunk chunk;
     initChunk(&chunk);
 
     int constant = addConstant(&chunk, 1.2);
+
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
-
     writeChunk(&chunk, OP_RETURN, 123);
+
     disassembleChunk(&chunk, "test chunk");
+
+    interpret(&chunk);
+
+    freeVM();
     freeChunk(&chunk);
 
     return 0;
---
+++ vm.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+#include "common.h"
+#include "debug.h"
+#include "vm.h"
+
+VM vm;
+
+void initVM()
+{
+}
+
+void freeVM()
+{
+}
+
+static InterpretResult run()
+{
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+
+    for (;;)
+    {
+#ifdef DEBUG_TRACE_EXECUTION
+        disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+#endif
+        uint8_t instruction;
+        switch (instruction = READ_BYTE())
+        {
+        case OP_CONSTANT:
+        {
+            Value constant = READ_CONSTANT();
+            printValue(constant);
+            printf("\n");
+            break;
+        }
+        case OP_RETURN:
+        {
+            return INTERPRET_OK;
+        }
+        }
+    }
+
+#undef READ_BYTE
+#undef READ_CONSTANT
+}
+
+InterpretResult interpret(Chunk *chunk)
+{
+    vm.chunk = chunk;
+    vm.ip = vm.chunk->code;
+    return run();
+}
