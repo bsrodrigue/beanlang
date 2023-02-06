e45a8d8feb6b2ce8311525dc7f88775400a6cb78
Rachid <rbadini@n7studio.com>
2023-02-06 17:21:24 +0000

    feature: lox scanner in c

--- vm.c
+++ vm.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "common.h"
+#include "compiler.h"
 #include "debug.h"
 #include "vm.h"
 
@@ -100,9 +101,8 @@ static InterpretResult run()
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk *chunk)
+InterpretResult interpret(const char *source)
 {
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
-    return run();
+    compile(source);
+    return INTERPRET_OK;
 }
