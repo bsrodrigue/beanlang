0285859dffaf14a4ad32b14d3613533e3be34723
Rachid <rbadini@n7studio.com>
2023-02-07 07:37:03 +0000

    feature: working on parsing and compiling basic expressions

--- compiler.h
+++ compiler.h
@@ -1,6 +1,41 @@
 #ifndef clox_compiler_h
 #define clox_compiler_h
 
-void compile(const char *source);
+#include "vm.h"
+#include "scanner.h"
+
+typedef enum
+{
+    PREC_NONE,
+    PREC_ASSIGNMENT, // =
+    PREC_OR,         // or
+    PREC_AND,        // and
+    PREC_EQUALITY,   // == !=
+    PREC_COMPARISON, // < > <= >=
+    PREC_TERM,       // + -
+    PREC_FACTOR,     // * /
+    PREC_UNARY,      // ! -
+    PREC_CALL,       // . ()
+    PREC_PRIMARY
+} Precedence;
+
+typedef void (*ParseFn)();
+
+typedef struct
+{
+    ParseFn prefix;
+    ParseFn infix;
+    Precedence precedence;
+} ParseRule;
+
+typedef struct
+{
+    Token current;
+    Token previous;
+    bool hadError;
+    bool panicMode;
+} Parser;
+
+bool compile(const char *source, Chunk *chunk);
 
 #endif
--- vm.c
+++ vm.c
@@ -103,7 +103,20 @@ static InterpretResult run()
 
 InterpretResult interpret(const char *source)
 {
-    // Generate bytecode for VM
-    compile(source);
-    return INTERPRET_OK;
+    Chunk chunk;
+    initChunk(&chunk);
+
+    if (!compile(source, &chunk))
+    {
+        freeChunk(&chunk);
+        return INTERPRET_COMPILE_ERROR;
+    }
+
+    vm.chunk = &chunk;
+    vm.ip = vm.chunk->code;
+
+    InterpretResult result = run();
+
+    freeChunk(&chunk);
+    return result;
 }
